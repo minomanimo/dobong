dd42995c24d89b8fd1ab457854faf3f1aa5b1ab1
minomanimo <101791449+minomanimo@users.noreply.github.com>
2022-11-17 15:23:25 +0900

    Add files via upload

---
+++ src/main/java/DAO/tmDTO.java
@@ -0,0 +1,19 @@
+package Beans;
+
+public class tmDTO {
+	private String text;
+	private int count;
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+}
